e7d2f1d312dd61b975c01bc473f543d1bbc390c0
“Nilde <nilde.benedetti@gmail.com>
2026-07-28 18:07:56 +0200

    Refactor data fetching by moving JSON handling to functions.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
-// get jsondata with file_get_contents
-$albumJsonData = file_get_contents('./data/albums.json');
-// Decodes JSON string into an associative array (true = array, false = object)
-$albums = json_decode($albumJsonData, true);
+require_once('./functions.php');
 
 ?>
 
