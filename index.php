180c1336c478b9daa9626a3cb088fa7769ac0d1f
“Nilde <nilde.benedetti@gmail.com>
2026-07-28 15:28:19 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Melancholia.fm</title>
+
+    <!-- Bootstrap CSS (v5.3.3) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    
+    <!-- Custom Stylesheet -->
+    <link rel="stylesheet" href="./style/index.css">
+</head>
+<body class="bg-mf-dark text-mf-main">
+
+    <header class="text-center py-5">
+        <h1 class="font-display text-mf-lavender text-glow-lavender fw-bold display-4">Melancholia.fm</h1>
+        <h5 class="font-lcd text-mf-cyan text-glow-cyan mt-2">Your favorite music player for nostalgic moments</h5>
+    </header>
+    <div class="container my-4">
+        <div class="lcd-display p-3 d-flex justify-content-between align-items-center">
+            <span class="font-lcd">> SYSTEM STATUS: READY</span>
+            <span class="font-lcd">[ TOTAL ALBUMS: 5 ]</span>
+        </div>
+    </div>
+    <!-- card container-->
+    <div class="container">
+        <ul>
+
+        </ul>
+    </div>
+
+    <!-- Bootstrap JS Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
